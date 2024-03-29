8c84a60dba2af2a861d963c36c7379da3d48a63c
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-03-29 17:55:00 +0100

    Evil Eevee

--- Eevee.bat
+++ Eevee.bat
@@ -1,7 +1,7 @@
 @echo off
 
-title Gurra optimus Evil Eevee
-color fc
+title {Gurra optimus Evil Eevee}
+color c fc
 echo Hello, do you love me? (answer in only yes/no)
 set /p input=
 if /i %input%==Yes goto love
