92130af8df36bbeb3e4fb68736e12e06c6552f0f
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2025-02-11 17:27:53 +0100

    shutdown your pc

--- shutdown.bat
+++ shutdown.bat
@@ -5,15 +5,15 @@ title shutdown
 COLOR 20
 echo do you want shutdown (yes/no)
 set /p input=
-if /i %input%==no goto love
-if /i %input%==Yes goto hate
+if /i %input%==no goto stil-on
+if /i %input%==Yes goto bye
 if /i not %input%== Yes,no goto 1
 
-:love
-echo Wellcomme back
+:stil-on
+echo Welcome back
 exit
 
-:hate
-echo bye See you Later
+:bye
+echo bye see you later
 timeout 3
 shutdown -s -t 10
